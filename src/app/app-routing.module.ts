4b7f09f5af7bf30e0becf6c1594a0ab24a74a1b4
EstebanJC <ejimenez@nediar.com>
2020-10-27 17:07:36 -0500

    project pages and componets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NavbarComponent, children: [
+    { path: 'Home', component: HomeComponent }
+  ] },
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'Home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PokemonDetailsComponent } from './pages/pokemon-details/pokemon-details.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PokeCardComponent } from './components/poke-card/poke-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PokemonDetailsComponent,
+    NavbarComponent,
+    PokeCardComponent
   ],
   imports: [
     BrowserModule,
